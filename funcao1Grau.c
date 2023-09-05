30623175a10da3e97e2e64c014e645b340289974
Léo Zambone <138814524+LeoZamboneNeto@users.noreply.github.com>
2023-09-05 19:20:55 -0300

    Update funcao1Grau.c
    Adição de biblioteca para acentuação em português.
    
    Modificação para colunas não ficarem em proporções de quebra pela diferença de tamanho dos números, ex 8.00 e 10.00, isso causava o pulo de um carácter da tabela.
    
    no loop: for(float i =....), antes estava como for(int i =....) e isso causava erro, pois todo o sistema do loop estava trabalhando com números floar

--- funcao1Grau.c
+++ funcao1Grau.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+
     float y, a, x, b, p;
     // y = ax + b; onde a != 0;
-    
     float xi; //valor inicial
     float xf; //valor final
     // p = (xf - xi) / 10;
-    
-    printf(" O programa constitui-se em uma função de primeiro grau 'y = ax + b'.\n Digite o valor de a, b, xi, xf - Com o mesmo padrão do exemplo, em 1 input separados por vírgula\n sendo; 'xi' = valor inicial e 'xf' = valor final\n");
-    printf(" Digite os valores a, b, xi, xf : ");   
-    scanf("%f, %f, %f, %f", &a, &b, &xi, &xf);
-    
-    
-    if( a == 0 ){
-        printf(" \nOs valores não serão possíveis para o cálculo pois a = 0, por favor, tente novamente");
 
-    }else; 
+    printf(" O programa constitui-se em uma função de primeiro grau 'y = ax + b'.\n Digite o valor de a, b, xi, xf - Com o mesmo padrão do exemplo, em 1 input separados por vírgula\n sendo; 'xi' = valor inicial e 'xf' = valor final\n");
+    printf(" Digite os valores a, b, xi, xf : ");
+    scanf("%f %f %f %f", &a, &b, &xi, &xf);
+
+
+    if( a == 0 || a == 0.0 ){
+        printf(" \nOs valores não serão possíveis para o cálculo pois a = 0, por favor, tente novamente");
+        return 1;
+    }else;
 
     printf(" \n Os valores escolhidos são: a = %.2f | b = %.2f | xi = %.2f | xf = %.2f |\n", a, b , xi, xf);
-    
+
     p = (xf - xi) / 10;
-    printf("   x \t |   y\n");
-    
-    for(int i = xi; i <= xf; i = i + p){
+    printf(" |  x     |   y\n");
+
+    for(float i = xi; i <= xf; i += p){
         y = a * i + b;
-       
-        printf(" | %.2i\t |   %3.2lf\n", i, y);
+
+        printf(" | %5.2f  | %5.2f\n", i, y);
     }
-    
-    
-    
     return 0;
 }
+
